9f736dcac142f64d440e05ffe786d437bcf98e04
hynduf <chauhuynhnoc@gmail.com>
2025-09-08 09:19:31 +0700

    Update to v1.15.5b

--- flake.nix
+++ flake.nix
@@ -9,10 +9,10 @@
     let
 
       system = "x86_64-linux";
-      version = "1.15.4b";
+      version = "1.15.5b";
       downloadUrl = {
         generic.url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
-        generic.sha256 = "0r644rx24xbsrn9js424iqb74zlg64a63rrrpl8dyx2sz7j2vh4v";
+        generic.sha256 = "0rychb5xhc1alr50mcf2lj6shy0shch8pqcxkvhylqgzdm6p4hwd";
       };
 
       pkgs = import nixpkgs {
