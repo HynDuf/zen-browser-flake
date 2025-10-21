ff414e8f8eaf0415f7ee2c18f4213ac4594ad7fa
hynduf <chauhuynhnoc@gmail.com>
2025-10-22 06:12:25 +0700

    Update to v1.7b

--- flake.nix
+++ flake.nix
@@ -9,10 +9,10 @@
     let
 
       system = "x86_64-linux";
-      version = "1.16.4b";
+      version = "1.17b";
       downloadUrl = {
         generic.url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
-        generic.sha256 = "1m2ba5011km8mrj3vxh991bypxqdgl5m02ab78p1rg8x1c45vjqc";
+        generic.sha256 = "08b64spxyjskdxr0x4aszwlqad4c1r5k8xh5zzfhwf0d00m7yx3c";
       };
 
       pkgs = import nixpkgs {
